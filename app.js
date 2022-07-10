2feff2300dc8e9e4c93c53641bd68e2265ae46df
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-10 12:26:12 +0530

    Added some event handlers for socket.io

--- app.js
+++ app.js
@@ -10,12 +10,21 @@ const io = new Server(httpServer, {
         origin: "*"
     }
 })
-
 io.on("connection", (socket) => {
+    const numberOfClients = io.engine.clientsCount;
+    console.log("Number of clients connected = ", numberOfClients);
     console.log("A user is connected = ", socket.id);
     socket.on("disconnect", (reason) => {
         console.log("User got disconnected");
         console.log("Reson = ", reason);
+    })
+    socket.on("create:todo", (arg) => {
+        console.log("Item send from todo:create event = ", arg);
+        socket.broadcast.emit("create:todo", arg);
+    })
+    socket.on("delete:todo", (index) => {
+        console.log("Item send from todo:delete event = ", index);
+        socket.broadcast.emit("delete:todo", index);
     })
 })
 
